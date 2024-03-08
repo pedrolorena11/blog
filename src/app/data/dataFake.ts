98c252800c7230f97423047223febedf6184c27e
Pedro <icm_pedro@hotmail.com>
2024-03-07 23:44:49 -0300

    finalizado

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,28 @@
+export const dataFake = [
+    {
+        "id":"1",
+        "title":"Deadpool & Wolverine, novo filme da Marvel, ganha primeiro trailer",
+        "description":
+        "A Marvel Studios divulgou, na noite deste domingo (11), durante o Super Bowl LVIII, o aguardadíssimo primeiro trailer de Deadpool & Wolverine, terceiro filme do Mercenário Tagarela, que marca a estreia do personagem vivido por Ryan Reynolds no Universo Cinematográfico da Marvel. Deadpool 3, que está previsto para estrear em 26 de julho de 2024, é dirigido por Shawn Levy, com quem Reynolds já trabalhou em Free Guy: Assumindo o Controle (2021) e O Projeto Adam (2022). Atualmente, o projeto está em fase de pós-produção, após encerrar as filmagens no fim de janeiro. Além de Reynolds, o novo longa da Marvel conta com os retornos de rostos já conhecidos pelos fãs da Marvel, como Hugh Jackman como Wolverine e Tyler Mane como Dentes de Sabre, ambos da franquia X-Men; e Jennifer Garner como Elektra Natchios, de Demolidor: O Homem Sem Medo (2003) e Elektra (2005). Filmado no Brasil, estrelado por artistas brasileiros e falado em português, Orfeu Negro venceu a maior premiação do cinema em 1960, mas o prêmio não veio para as estantes brasileiras. Personagens dos dois filmes anteriores do Mercenário Tagarela também estão de volta no terceiro filme, como Morena Baccarin como Vanessa, Stefan Kapicic como Colosso, Brianna Hildebrand como Míssil Adolescente Megassônico, Shioli Kutsuna como Yukio, Rob Delaney como Peter, Karan Soni como Dopinder e Leslie Uggams como Al. Por fim, Emma Corrin (The Crown) e Matthew Macfadyen (Succession) são as novas adições ao Universo Cinematográfico da Marvel, em papéis ainda não revelados.",        
+        "photoCover":"https://rollingstone.uol.com.br/media/_versions/2024/02/deadpool-wolverine-novo-filme-da-marvel-ganha-primeiro-trailer-5_widelg.jpg"
+    },
+    {
+        "id":"2",
+        "title":"Ryan Reynolds dá uma ótima notícia aos fãs da Marvel e de Deadpool",
+        "description":"Os fãs da Marvel podem estar apreensivos em 2024, já que o ano será o com o menor número de filmes do MCU desde 2012: apenas um só. Porém, naquele ano o filme solitário foi a reunião de todos os heróis que já tinham aparecido no universo compartilhado criado em Homem de Ferro e agora o único filme MCU do ano será justamente um que veio de outro universo: Deadpool. Mas Ryan Reynolds tem uma boa notícia para os fãs.Depois de enfrentar a compra da Fox, o retorno dos direitos para a Disney, uma greve dos atores e qualquer outra adversidade, enfim o terceiro filme do mercenário tagarela terminou de ser filmado. Reynolds confirmou o encerramento das gravações em um post no Instagram. 'O traje esconde sangue. Também (esconde) suor… mas hoje, com o final das gravações de Deadpool, são principalmente lágrimas. Um agradecimento gigante e eterno ao elenco e à equipe do nosso filme que lutou contra o vento, a chuva, as greves e Hugh Jackman… tudo sob a forte liderança de Shawn Levy', diz a primeira parte do post.Fiz um filme com meus amigos mais próximos e isso não acontece com muita frequência. Até 26 de julho', completou o ator ao lembrar da data de estreia do filme nos cinemas dos Estados Unidos. Como no Brasil os filmes estreiam de quinta-feira, o filme deve chegar aos cinemas em 25 de julho.",
+        "photoCover":"https://cinebuzz.uol.com.br/media/uploads/shawn_levy_diretor_de_deadpool_3_explicou_o_que_o_levou_a_escolher_o_traje_classico_do_wolverine_para_o_filme.jpg"
+    },
+
+    {
+        "id":"3",
+        "title":"Hugh Jackman volta como Wolverine em foto do filme",
+        "description":"A primeira imagem de Hugh Jackman como Wolverine em Deadpool 3 foi divulgada nesta segunda-feira (10). O ator Ryan Reynolds compartilhou a foto no Instagram. Foi ele também que anunciou em setembro do ano passado que o filme teria Jackman de volta ao papel.Em vídeo publicado no perfil de Reynolds no Twitter, o ator canadense contou com participação relâmpago do australiano, que interpretou o herói mutante por 17 anos nos cinemas.'Logan'(2017) era, até agora, sua despedida do personagem.",
+        "photoCover":"https://cinebuzz.uol.com.br/media/uploads/shawn_levy_diretor_de_deadpool_3_explicou_o_que_o_levou_a_escolher_o_traje_classico_do_wolverine_para_o_filme.jpg"
+    },
+    {
+        "id":"4",
+        "title":"'Vai salvar o MCU', diz diretor de X-Men sobre Deadpool 3",
+        "description":"Com estreia marcada para o dia 26 de julho, Deadpool 3 é a grande aposta da Marvel para este ano. E se depender de Matthew Vaughn, que dirigiu X-Men: Primeira Classe, o novo filme será um grande sucesso. Segundo ele, o longa 'vai salvar o MCU (Universo Cinematográfico Marvel)'.     'O pouco que eu sei sobre Deadpool vs. Wolverine – ou talvez seja Wolverine vs. Deadpool, com certeza Ryan [Reynolds] e Hugh [Jackman] estão tendo essa discussão no momento – é inacreditável. Vai ser um choque… o universo Marvel vai levar um choque, e acho que vai ser o choque necessário para ressuscitá-lo. Acho que Ryan e Hugh estão prestes a salvar o MCU.' ",
+        "photoCover":"https://proxy.olhardigital.com.br/wp-content/uploads/2023/07/Deadpool-2016-Fox-1024x614.jpg"
+    }
+]
